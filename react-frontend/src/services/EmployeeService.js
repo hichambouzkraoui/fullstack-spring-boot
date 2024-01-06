247eee7e5d2ee7c43636efbcd4279d4a34cb102b
Hicham Bouzkraoui <hichamm.bouzkraoui@gmail.com>
2024-01-05 20:26:57 -0700

    add ECS deploy

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "http://18.214.25.22:8080/api/v1/employees";
 
 class EmployeeService {
 
